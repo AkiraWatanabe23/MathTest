e8df660b31d3bd821cc152dce387fdc69d5919d6
Akira Watanabe <a0323w@gmail.com>
2022-12-15 20:02:57 +0900

    Rigidbodyを使わない当たり判定を考える

--- Assets/Scripts/SlopeMove2D.cs
+++ Assets/Scripts/SlopeMove2D.cs
@@ -16,34 +16,10 @@ public class SlopeMove2D : MonoBehaviour
     {
         var hol = Input.GetAxisRaw("Horizontal");
         Transform trans = transform;
-
-        Vector2 delta = transform.position - _target.position;
-        float dot = Vector3.Dot(Mul(_target.position, Vector3.right), delta) / 
-            Vector3.SqrMagnitude(Mul(_target.position, Vector3.right));
-
-        if (dot < -1f)
-        {
-            dot = -1f;
-        }
-        else if (dot > 1f)
-        {
-            dot = 1f;
-        }
-        Vector3 minPos = Mul(_target.position, Vector3.right) * dot + _target.position;
-
-        float sqr = Vector3.SqrMagnitude(minPos - transform.position);
-        float arg = sqr * sqr;
-
-
+        
         Vector2 pos = trans.position;
         pos.x += 0.01f * hol;
 
         trans.position = pos;
-    }
-
-    //Vector3“¯Žm‚ÌŠ|‚¯ŽZ
-    public static Vector3 Mul(Vector3 a, Vector3 b)
-    {
-        return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
     }
 }
